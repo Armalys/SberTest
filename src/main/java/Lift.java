6ea91cd2dc18e4f8358cb8a330bc4148ee2f29fb
Armalys <mashnert@mail.ru>
2020-02-18 17:09:24 +0300

    renaming variables

--- src/main/java/Lift.java
+++ src/main/java/Lift.java
@@ -5,8 +5,8 @@ import java.util.TreeSet;
 public class Lift {
 
     static int numberOfFloors;
-    static int timeGoUpOnFoot;
-    static int timeGoDownOnFoot;
+    static int timeGoUpOnFloor;
+    static int timeGoDownOnFloor;
     static int liftTime;
 
     public static void main(String[] args) {
@@ -15,9 +15,9 @@ public class Lift {
         System.out.print("Введите количество этажей: ");
         numberOfFloors = scanner.nextInt();
         System.out.print("Введите время необходимо на подъем человека на 1 этаж пешком: ");
-        timeGoUpOnFoot = scanner.nextInt();
+        timeGoUpOnFloor = scanner.nextInt();
         System.out.print("Введите время необходимо на спуск человека на 1 этаж пешком: ");
-        timeGoDownOnFoot = scanner.nextInt();
+        timeGoDownOnFloor = scanner.nextInt();
         System.out.print("Введите время которое необходимо лифту для перемещения на 1 этаж: ");
         liftTime = scanner.nextInt();
 
@@ -29,22 +29,22 @@ public class Lift {
     public int receiveTimeWithUseLift(int currentFloor, int floorToUp) {
         SortedSet<Integer> maxTimeToEnterFloor = new TreeSet<>();
         //Для тех, кто едет в в лифте, затем поднимается
-        maxTimeToEnterFloor.add(currentFloor * liftTime + (numberOfFloors - currentFloor) * timeGoUpOnFoot);
+        maxTimeToEnterFloor.add(currentFloor * liftTime + (numberOfFloors - currentFloor) * timeGoUpOnFloor);
         //Время для тех кто спускается поле лифта
-        maxTimeToEnterFloor.add(currentFloor * liftTime + (currentFloor - floorToUp - 1) * timeGoDownOnFoot);
+        maxTimeToEnterFloor.add(currentFloor * liftTime + (currentFloor - floorToUp - 1) * timeGoDownOnFloor);
         //Время для тех, кто не едет в лифте
-        maxTimeToEnterFloor.add(floorToUp * timeGoUpOnFoot);
+        maxTimeToEnterFloor.add(floorToUp * timeGoUpOnFloor);
         return maxTimeToEnterFloor.last();
     }
 
     public int receiveTotalResultTime() {
         SortedSet<Integer> allTimeResults = new TreeSet<>();
         int floorToUp = 0;
-        int totalResult = numberOfFloors * timeGoUpOnFoot;
+        int totalResult = numberOfFloors * timeGoUpOnFloor;
         allTimeResults.add(totalResult);
         for (int currentFloor = 1; currentFloor <= numberOfFloors; currentFloor++) {
             // Этаж, на котором встретятся люди которые поднимаются и люди которые спускаются
-            floorToUp = (liftTime + timeGoDownOnFoot) * currentFloor / (timeGoUpOnFoot + timeGoDownOnFoot);
+            floorToUp = (liftTime + timeGoDownOnFloor) * currentFloor / (timeGoUpOnFloor + timeGoDownOnFloor);
             allTimeResults.add(receiveTimeWithUseLift(currentFloor, floorToUp));
             totalResult = allTimeResults.first();
         }
